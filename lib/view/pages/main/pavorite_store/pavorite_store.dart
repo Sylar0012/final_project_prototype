caae43b3dce1c21dd584f22139628405d6a9d11a
sylar0012 <kmc42692@naver.com>
2022-11-24 09:15:13 +0900

    text

--- lib/view/pages/main/pavorite_store/pavorite_store.dart
+++ lib/view/pages/main/pavorite_store/pavorite_store.dart
@@ -9,7 +9,7 @@ class PavoriteStore extends StatelessWidget {
       color: Colors.blue[300],
       child: Center(
         child: Text(
-          "찜한 가게",
+          "찜한 가게fdfsafdsa",
           style: TextStyle(
             color: Colors.white,
             fontSize: 50,
